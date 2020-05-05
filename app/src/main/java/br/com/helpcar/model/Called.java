b5bf23490ddacf4a21ad4ecdfdb06cf05f655a76
marcosSamapio <vinicius.marcos.mvvlk@gmail.com>
2020-05-04 22:15:54 -0300

    Merge

--- app/src/main/java/br/com/helpcar/model/Called.java
+++ app/src/main/java/br/com/helpcar/model/Called.java
@@ -12,14 +12,18 @@ public class Called {
     private String modelVehicle;
     private String calledDescription;
     private String photoOfVehicle;
+    private Double latitude;
+    private Double longitude;
 
     @Ignore
-    public Called(int calledId, String kindOfVehicle, String brandVehicle, String modelVehicle, String calledDescription, String photoOfVehicle) {
+    public Called(int calledId, String brandVehicle, String modelVehicle, String calledDescription, String photoOfVehicle, Double latitude, Double longitude) {
         this.calledId = calledId;
         this.brandVehicle = brandVehicle;
         this.modelVehicle = modelVehicle;
         this.calledDescription = calledDescription;
         this.photoOfVehicle = photoOfVehicle;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public Called() {
@@ -75,5 +79,21 @@ public class Called {
                 ", problemDescription='" + calledDescription + '\'' +
                 ", photoOfVehicle='" + photoOfVehicle + '\'' +
                 '}';
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 }
